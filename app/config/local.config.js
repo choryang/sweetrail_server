36b1714cbe5f6d72c38506d0dedbe9ec683d803a
choryang <okee0815@gmail.com>
2020-11-12 16:42:35 +0900

    add local config file

---
+++ app/config/local.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "sictc@202042",
+    DB: "postgres",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  };
+
+  module.exports = {
+    HOST: "localhost",
+    USER: "okee",
+    PASSWORD: "password",
+    DB: "postgres",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  };
