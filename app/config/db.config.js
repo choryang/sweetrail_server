90b05fc73afc421ae2b2d0272dad8acf33185c1e
choryang <okee0815@gmail.com>
2020-11-26 01:06:16 +0900

    db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "localhost",
-  USER: "okee",
-  PASSWORD: "password",
+  USER: "postgres",
+  PASSWORD: "sictc@202042",
   DB: "postgres",
   dialect: "postgres",
   pool: {
@@ -12,19 +12,7 @@ module.exports = {
   },
 };
 
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "postgres",
-//   PASSWORD: "sictc@202042",
-//   DB: "postgres",
-//   dialect: "postgres",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
+
 
 
 
